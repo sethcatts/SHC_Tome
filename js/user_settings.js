9332860bfffb837f988a782adb5d19078afc0d04
Seth <seth.h.catts@gmail.com>
2020-06-09 16:43:19 -0400

    op sec

---
+++ js/user_settings.js
@@ -0,0 +1,37 @@
+const siteDict = {
+    48:"http://192.168.0.1",            //0
+    49:"https://boards.4chan.org/wsg/", //1
+    50:"https://boards.4chan.org/g/",   //2
+    51:"https://boards.4chan.org/wg/",  //3
+    52:"https://boards.4chan.org/gd/",  //4
+    53:"https://boards.4chan.org/gif/", //5
+    55:"file:///F:/",                   //8
+    56:"file:///C:/",                   //9
+    65:"https://www.audible.com",       //A
+    66:"http://www.google.com",         //B 
+    67:"http://www.lichess.org",        //C                
+    68:"https://drive.google.com",      //D
+    69:"http://www.gmail.com",          //E
+    70:"https://freecodecamp.org",      //F
+    71:"http://www.github.com",         //G
+    72:"http://www.stackoverflow.com",  //H
+    74:"https://www.w3schools.com",     //J
+    78:"https://app.codesignal.com",    //N
+    81:"http://www.google.com",         //Q
+    84:"https://todoist.com",           //T
+    87:"http://www.twitch.tv",          //W
+    89:"https://www.youtube.com",       //Y
+    112:"https://www.youtube.com/watch?v=u0ZTcAOP9kk",                              //F1
+    113:"https://www.youtube.com/watch?v=RDhqko-Ivi0",                              //F2
+    114:"https://www.youtube.com/watch?v=8KAWnFLvDXA",                              //F3
+    115:"https://www.youtube.com/watch?v=YWRTbxvGO3c",                              //F4
+    116:"https://www.youtube.com/playlist?list=PLLyULqS39DP-PfyjiEL_0MK0ZXp0CnuIk"  //F5
+}
+
+const quotes = [];
+
+var user_settings = {
+    "name":"James Smith",
+    "siteDict":siteDict,
+
+}
