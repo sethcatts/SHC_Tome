157b25a3c99e0a6b7716a0f75dca914b5c007a8e
Seth <seth.h.catts@gmail.com>
2021-05-29 01:47:23 -0400

    semi-completing v2.0

---
+++ getImageList.py
@@ -0,0 +1,8 @@
+import os
+import glob
+
+path = r"F:\Programming & Computer Science\Projects\SHC_Tome\images\stingers"
+
+images = os.listdir(path)
+
+print(images)
--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,14 @@ function randomWall() {
     document.getElementById('pageQuote').innerHTML = selectedQuote;
 }
 
+function randomStinger() {
+    var imageList = ['stinger (1).gif', 'stinger (1).jpg', 'stinger (1).png', 'stinger (10).JPG', 'stinger (11).JPG', 'stinger (12).JPG', 'stinger (13).JPG', 'stinger (14).JPG', 'stinger (15).jpg', 'stinger (16).jpg', 'stinger (17).JPG', 'stinger (18).jpg', 'stinger (19).jpg', 'stinger (2).jpg', 'stinger (2).PNG', 'stinger (20).jpg', 'stinger (21).jpg', 'stinger (22).jpg', 'stinger (23).jpg', 'stinger (24).jpg', 'stinger (25).jpg', 'stinger (26).jpg', 'stinger (3).jpg', 'stinger (3).PNG', 'stinger (4).JPG', 'stinger (4).png', 'stinger (5).jpg', 'stinger (5).PNG', 'stinger (6).JPG', 'stinger (6).png', 'stinger (7).JPG', 'stinger (7).png', 'stinger (8).JPG', 'stinger (8).png', 'stinger (9).JPG', 'stinger (9).png']
+    var selectedImage = "url('/images/stingers/" + imageList[Math.floor(Math.random() * imageList.length)] + "')";
+    var stingerDiv = document.getElementById('stinger-image');
+    console.log(selectedImage)
+    stingerDiv.style.backgroundImage = selectedImage;
+}
+
 function updateClock() {
   var currentTime = new Date();
   var currentHours = currentTime.getHours();
@@ -39,13 +47,13 @@ function updateClock() {
   currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
   // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  var currentTimeString = currentHours + ":" + currentMinutes + " " + timeOfDay;
 
   // Update the time display
   document.getElementById("clock").innerHTML = currentTimeString;
 }
 
 randomQuote();
-//randomWall();
+randomStinger();
 updateClock();
 setInterval('updateClock()', 1000);
