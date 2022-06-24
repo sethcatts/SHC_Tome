81c9c534498a4fe737ddb5134b150218215e78aa
Seth Catts <seth.h.catts@gmail.com>
2022-06-24 15:07:50 -0400

    Removing some old code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
-document.onkeydown = handleKeyPress;
 console.log(user_settings.name);
-
-//Orgasmicly simple code
-function handleKeyPress(e) {
-    e = e || window.event;
-    if (e.keyCode in siteDict) {
-        window.open(user_settings.siteDict[e.keyCode], target = "_self");
-    }
-}
 
 function randomQuote() {
     var selectedQuote = user_settings.quotes[Math.floor(Math.random() * user_settings.quotes.length)];
